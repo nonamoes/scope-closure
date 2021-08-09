d3606ae44a5cbeb7d2f9cf66786ebfc709b5d646
nonamoes <chiusagui@gmail.com>
2021-08-09 14:14:02 -0500

    hoisting
    Se creo un ejemplo de hoisting con variables y con una funcion

---
+++ src/hoisting/index.js
@@ -0,0 +1,18 @@
+a =2;
+var a;
+console.log(a);  //imprime 2
+
+
+//en este ejemplo hay un error, pero como a no esta definia primero dara error: undifined
+console.log(a);
+var a = 2;
+
+
+//creamos una funcion
+
+nameOfDog("Elmo"); //imprime Elmo
+
+function nameOfDog(name){
+    console.log(name);
+}
+
